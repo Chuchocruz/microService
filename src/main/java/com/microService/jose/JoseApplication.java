f442ed55229762334e566612e2f0bcb448f2a877
josecg <jcruzg1605@alumno.ipn.mx>
2025-05-01 08:17:57 -0600

    segundo commit

--- src/main/java/com/microService/jose/JoseApplication.java
+++ src/main/java/com/microService/jose/JoseApplication.java
@@ -3,8 +3,18 @@ package com.microService.jose;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 @SpringBootApplication
 public class JoseApplication {
+
+	@GetMapping("/weatherForecast")
+	public String getMessage() {
+		return "Hoy es un gran dia soleado desde el servicio Java";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JoseApplication.class, args);
